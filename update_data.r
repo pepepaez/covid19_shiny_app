376af31c7b822dad2637b296b497f73804ff2e73
pepepaez <jopaez@gmail.com>
2020-06-10 19:06:25 -0700

    changes prior to v2

--- update_data.r
+++
@@ -1,7 +0,0 @@
-temp <- tempfile()
-download.file("http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip",temp)
-file_name <- unzip(temp, list = TRUE)$Name[1]
-datos <- read.table(unz(temp, file_name), sep = ",", header = TRUE)
-unlink(temp)
-save(datos, file = "./data/datos.Rdata")
-save.image()
