42136dd2506479e9195b72338fc0eabc9837ca92
pepepaez <jopaez@gmail.com>
2020-06-12 17:56:58 -0700

    cambios a script para actualizar informacion

--- update_data.r
+++ update_data.r
@@ -26,7 +26,7 @@ load("./data/por_captura.Rdata")
 colnames(hoy)[1]<-"FECHA_CAPTURA"
 hoy <- hoy %>% group_by(FECHA_CAPTURA, ENTIDAD_RES, MUNICIPIO_RES, RESULTADO) %>% summarise(casos = sum(T), mujeres = sum(SEXO == 1), hombres = sum(SEXO == 2), sexo_no_especificado = sum(SEXO == 99), ambulatorios = sum(TIPO_PACIENTE == 1), hospitalizado = sum(TIPO_PACIENTE == 2), otro = sum(TIPO_PACIENTE == 99), intensivo = sum(UCI == 1), activos = sum(as.Date(FECHA_SINTOMAS) >= as.Date(FECHA_CAPTURA) - days(13) ))
 hoy <- hoy %>% mutate(FECHA_CAPTURA = as.Date(FECHA_CAPTURA, "%Y-%m-%d")) %>% arrange(FECHA_CAPTURA)
-por_captura <- rbind(por_captura, hoy_temp)
+por_captura <- rbind(por_captura, hoy)
 
 ## Guarda la informacion
 save(por_captura, file="./data/por_captura.Rdata")
